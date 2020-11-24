f399c00b94d98b7aea4337f103f6061340fa6c87
maytheu <maytheu98@gmail.com>
2020-11-24 18:13:31 +0100

    updating Readme, addihg moongoose and body-parser, updating views to add short url by the user and configuring the server to accept string variable

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 
 # A modified version of the freecodecamp url-shortener-microservice to accept user variables as optional parameters for url
+
+#Apologies in advance, the code looks rough just because i uaed my phone to write the code
--- server.js
+++ server.js
@@ -3,12 +3,38 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const dns = require("dns")
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+mongoose.connect(process.env.DB_URL,{useNewUrlParser: true, useUnifiedTopology: true })
+
+
+//mongoose schema
+ const urlSchema = mongoose.Schema({
+   originalUrl:{
+     type: String,
+     required:true,
+     unique: true
+   },
+   shortUrl:{
+     type: String,
+     unique: true
+   }
+ })
+
+ //model
+ const Url = mongoose.model("Url", urlSchema)
+
+//configure bodyParser
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -18,6 +44,57 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post("/api/shorturl/new", (req, res)=>{
+ let url = req.body.url
+	let shortUrl = req.body.short_url.toLowerCase()
+	console.log(shortUrl==""?1:2)
+ //check mongoose connection =  1 fir connection
+ console.log(mongoose.connection.readyState)
+
+//if the user do not enter a string, gejerateva random string
+	let ranUrl = 
+	let result = url.replace(/(^\w+:|^)\/\//ig, "").split(/[/?#]/)[0]
+ console.log(result)
+
+	dns.lookup(result, (err, address, family) => {
+    console.log(err)
+   if(err) return res.json({error: 'invalid url'})
+   
+   //find the last short url in the database
+   /*Url.find({}).limit(1).sort({shortUrl: -1}).select('shortUrl').exec((err, data)=>{
+        if (err) return res.json({error:err})
+        let short_url= data[0].shortUrl+1*/
+
+	//using stribg variabkes instead of nunbers
+	//find the DB to nake sure that the stribgvis uniwue befire snding it to the Db
+	Url.find({}).exec((err, data)=>{
+	if (err) return res.json({error:err})
+		data.map(short =>{
+			console.log(short.shortUrl)
+		if(shortUrl !== short.shortUrl){
+		return shortUrl
+		}else{
+			shortUrl = "Short Url Exist"}
+		})
+		if(shortUrl === "Short Url Exist"){
+   res.json({short_url: "Short Url Exist"})
+		} else{
+   //instance to be saved in DB
+   const shortDom = new Url({originalUrl: url, shortUrl:shortUrl})
+   
+   shortDom.save((err, domain)=>{
+     if (err) return res.json({error: err})
+   
+   
+   res.json({original_url:url, short_url:shortUrl})
+   
+  console.log('address: %j family: IPv%s', address, family)
+  })
+}
+   })
+});
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
